34993cc12adbc923978bc85ff7445e0746395939
AveryH93 <hogan970@wearescpps.org>
2025-09-08 15:26:37 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,11 +27,15 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+      createPlatform(125, 675, 100, 25, "black");
+      createPlatform(425, 700, 70, 15, "rgba(27, 33, 33, 1)");
+      createPlatform(625, 570, 35, 50, "rgba(210, 180, 140, 1)");
+      createPlatform(415, 445, 70, 20, "rgba(238, 215, 122, 1)");
+      createPlatform();
 
 
 
